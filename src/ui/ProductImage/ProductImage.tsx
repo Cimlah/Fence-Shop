9c4c03d168d1b7e3e41c32ce477a88b175fa6a3c
Michał Stolarczyk <cimlahcimlah@gmail.com>
2023-09-21 23:54:24 +0200

    Added styles for `ProductImage` component

--- src/ui/ProductImage/ProductImage.tsx
+++ src/ui/ProductImage/ProductImage.tsx
@@ -1,10 +1,12 @@
-type ImageProps = {
+import styles from './ProductImage.module.css';
+
+export type ImageProps = {
     source: string
     imageAlt: string
 }
 
 export function ProductImage({ imageAlt, source }: ImageProps) {
     return(
-        <img src={source} alt={imageAlt} />
+        <img className={styles.image} src={source} alt={imageAlt} />
     )
 }
